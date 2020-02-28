d4d4543ca8a84987a988eda6f81423773d718c39
PRoman93 <roman_prokopets@mail.ua>
2020-02-28 11:49:53 +0200

    Updates

---
+++ precache-manifest.0de193b254cfa077a76e498facff9df0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a29500948191abf6b5fefa8020cddb0",
+    "url": "/dog-game/index.html"
+  },
+  {
+    "revision": "3a1450924eb01c9e52fa",
+    "url": "/dog-game/static/css/main.e05cb6fb.chunk.css"
+  },
+  {
+    "revision": "87ae0b893c5d4ee3f9c2",
+    "url": "/dog-game/static/js/2.956de5d5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/dog-game/static/js/2.956de5d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a1450924eb01c9e52fa",
+    "url": "/dog-game/static/js/main.9659115d.chunk.js"
+  },
+  {
+    "revision": "ec66d3bc7fbd4d2b6c19",
+    "url": "/dog-game/static/js/runtime-main.57330ac4.js"
+  },
+  {
+    "revision": "75fa68d4d95a95385dabee5a76f50797",
+    "url": "/dog-game/static/media/dog.75fa68d4.mp3"
+  }
+]);
